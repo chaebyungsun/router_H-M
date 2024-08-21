23e149aa45b1b7076da491e4085a33457db86bde
chaebyungsun <rhemdgkrtod3@naver.com>
2024-08-22 04:39:19 +0900

    first commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,48 @@
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import React from "react";
+
+const Navbar = () => {
+  const menuList = [
+    "여성",
+    "Divided",
+    "남성",
+    "유아",
+    "아동",
+    "H&M Home",
+    "Sale",
+  ];
+  return (
+    <div>
+      <div>
+        <div className="login_button">
+          <FontAwesomeIcon icon={faUser} />
+          <div>로그인</div>
+        </div>
+      </div>
+      <div className="section">
+        <img
+          width={100}
+          src="https://i.namu.wiki/i/DgtLbpNdB7yeqwJKvq5_o2qRRYJH6J6wMoWtgc5SIZhGeBQqEJDUEhGmQq2-kMhtCuGBbmSd8u0MMz2Q72JGjluU1IZs6TbKx3--eTG4bjbTRTomKD3IL--G3Z0ZTTKBJUx1pIvuxT8s2hx9N_jrtg.svg"
+          alt=""
+        />
+      </div>
+      <div className="menu">
+        <ul className="menu_list">
+          {menuList.map((menu) => (
+            <li>{menu}</li>
+          ))}
+        </ul>
+
+        <div className="search">
+          <FontAwesomeIcon icon={faSearch} />
+          <input type="text" placeholder="검색" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/component/ProductCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img
+        src="https://lp2.hm.com/hmgoepprod?set=source[/ac/36/ac36f6033a3b300bc0114d8f4e6ea28f5424d339.jpg],origin[dam],category[],type[LOOKBOOK],res[w],hmver[1]&call=url[file:/product/main]"
+        alt=""
+      />
+      <div>벨티드 트윌 코트</div>
+      <div>₩119,000</div>
+    </div>
+  );
+};
+
+export default ProductCard;
